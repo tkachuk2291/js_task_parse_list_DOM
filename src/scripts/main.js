b882c963c49b6acd60f49d03985c05202e3b20a2
Anton Tkachuk <tkacuk2291@gmail.com>
2024-12-26 17:20:22 +0200

    sol1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,30 @@
 'use strict';
 
-let lst = document.querySelectorAll('li');
+const lst = document.querySelectorAll('li');
 
-function getAttributeData(list) {
-  return Array.from(list)
-    .map(element => ({
-      name: element.textContent.trim(),
-      position: element.getAttribute("data-position"),
-      salary: Number(element.getAttribute('data-salary').slice(1).replace(/,/g, '')),
-      age: element.getAttribute("data-age")
-    })).sort((element1, element2) => element2.salary - element1.salary);
+function sortList(list) {
+  return Array.from(list).map((element) => ({
+    name: element.textContent.trim(),
+    position: element.getAttribute('data-position'),
+    salary: Number(
+      element.getAttribute('data-salary').slice(1).replace(/,/g, ''),
+    ),
+    age: element.getAttribute('data-age'),
+  }));
 }
 
-const Newlist = getAttributeData(lst);
-console.log(Newlist)
-GetEmployees(Newlist)
+const Newlist = sortList(lst);
 
-function GetEmployees(list) {
-  for (let [index, num] of Array.from(document.getElementsByTagName('li')).entries()) {
-    console.log(index, num);
-    num.innerHTML = list[index].name
+getEmployees(Newlist);
+
+function getEmployees(list) {
+  const NewList = list.sort(
+    (element1, element2) => element2.salary - element1.salary,
+  );
+
+  for (const [index, num] of Array.from(
+    document.getElementsByTagName('li'),
+  ).entries()) {
+    num.innerHTML = NewList[index].name;
   }
 }
