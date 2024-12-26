63124c562a2da01de5b46279a220d1e3e3450519
Anton Tkachuk <tkacuk2291@gmail.com>
2024-12-26 14:36:58 +0200

    sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+let lst = document.querySelectorAll('li');
+
+function getAttributeData(list) {
+  return Array.from(list)
+    .map(element => ({
+      name: element.textContent.trim(),
+      position: element.getAttribute("data-position"),
+      salary: Number(element.getAttribute('data-salary').slice(1).replace(/,/g, '')),
+      age: element.getAttribute("data-age")
+    })).sort((element1, element2) => element2.salary - element1.salary);
+}
+
+const Newlist = getAttributeData(lst);
+console.log(Newlist)
+GetEmployees(Newlist)
+
+function GetEmployees(list) {
+  for (let [index, num] of Array.from(document.getElementsByTagName('li')).entries()) {
+    console.log(index, num);
+    num.innerHTML = list[index].name
+  }
+}
